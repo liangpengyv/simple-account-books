815a3934521359d7777d54e42809a516a71cd607
liangpengyv <me@laoliang.ink>
2022-11-11 00:18:25 +0800

    chore: 整理账单统计相关代码

--- src/views/List.vue
+++ src/views/List.vue
@@ -13,14 +13,24 @@ const categoryStore = useCategoryStore()
 const billDateLoading = ref(false)
 const currentList = ref(null)
 
-const currentExpenditure = ref(0)
-const currentIncome = ref(0)
+const currentExpenditureTotal = ref(0)
+const currentIncomeTotal = ref(0)
 
 const currentMonthAnyTimestamp = ref(Date.now())
 const currentCategoryFilterList = ref([])
 
+const updateCurrentTotal = () => {
+  currentExpenditureTotal.value = 0
+  currentIncomeTotal.value = 0
+  currentList.value.forEach(item => {
+    if (item.type === 0) {
+      currentExpenditureTotal.value += item.amount
+    } else {
+      currentIncomeTotal.value += item.amount
+    }
+  })
+}
 const updateList = () => {
-  console.log('update...')
   const params = {
     startTime: getMonthStart(currentMonthAnyTimestamp.value).getTime(),
     endTime: getMonthEnd(currentMonthAnyTimestamp.value).getTime(),
@@ -30,15 +40,7 @@ const updateList = () => {
   listService.getList(params).then(res => {
     billDateLoading.value = false
     currentList.value = res
-    currentExpenditure.value = 0
-    currentIncome.value = 0
-    currentList.value.forEach(item => {
-      if (item.type === 0) {
-        currentExpenditure.value += item.amount
-      } else {
-        currentIncome.value += item.amount
-      }
-    })
+    updateCurrentTotal()
   })
 }
 updateList()
@@ -62,10 +64,9 @@ const jumpCurrentMonth = () => {
   <br />
 
   支出：￥
-  <n-number-animation ref="numberAnimationInstRef" :from="0.0" :to="currentExpenditure" :precision="2"
-    :duration="300" />
+  <n-number-animation :from="0.0" :to="currentExpenditureTotal" :precision="2" :duration="300" />
   收入：￥
-  <n-number-animation ref="numberAnimationInstRef" :from="0.0" :to="currentIncome" :precision="2" :duration="300" />
+  <n-number-animation :from="0.0" :to="currentIncomeTotal" :precision="2" :duration="300" />
 
   <n-skeleton v-if="billDateLoading" text :repeat="5" />
   <ul v-else-if="currentList && currentList.length > 0">
