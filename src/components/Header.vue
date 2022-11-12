e6747c8e3214dd26ac094b7a5b8e8211c6ad64eb
liangpengyv <me@laoliang.ink>
2022-11-12 11:25:58 +0800

    feature: 添加全局页头样式

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,41 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router'
+import { NButton, NIcon } from 'naive-ui';
 
 const route = useRoute()
 const router = useRouter()
 </script>
 
 <template>
-  <button v-if="route.name !== 'list'" @click="router.back">返回</button>
-  <h1>{{ route.meta.title }}</h1>
+  <div class="wrapper">
+    <n-button class="back-button" v-if="route.name !== 'list'" @click="router.back" text>
+      <n-icon size="20">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24">
+          <path d="M17.77 3.77L16 2L6 12l10 10l1.77-1.77L9.54 12z" fill="currentColor"></path>
+        </svg>
+      </n-icon>
+    </n-button>
+    <span class="title">{{ route.meta.title }}</span>
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  width: 100%;
+  height: 48px;
+  text-align: center;
+  position: relative;
+}
 
+.back-button {
+  position: absolute;
+  left: 0;
+  width: 48px;
+  height: 48px;
+}
+
+.title {
+  font-size: 20px;
+  line-height: 48px;
+}
 </style>
