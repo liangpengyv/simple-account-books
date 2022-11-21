b96263f33bfeaf4284d46f3741641425eed7f1c4
liangpengyv <me@laoliang.ink>
2022-11-22 02:18:53 +0800

    perf: 分析打包产物并拆分 chunk

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    visualizer()
+  ],
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-lib': ['vue', 'pinia', 'vue-router'],
+          'naive-ui-lib': ['date-fns', 'date-fns-tz', 'vueuc', 'lodash-es'],
+          'naive-ui': ['naive-ui']
+        }
+      }
+    }
+  }
 })
